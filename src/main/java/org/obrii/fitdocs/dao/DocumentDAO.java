19d7ce6c634e920245bce2807f5a514d7aa6847e
jurilents <keeeneeek@gmail.com>
2021-05-10 14:22:10 +0300

    Rename package entities to entity

--- src/main/java/org/obrii/fitdocs/dao/DocumentDAO.java
+++ src/main/java/org/obrii/fitdocs/dao/DocumentDAO.java
@@ -1,6 +1,6 @@
 package org.obrii.fitdocs.dao;
 
-import org.obrii.fitdocs.entities.Document;
+import org.obrii.fitdocs.entity.Document;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DocumentDao extends CrudRepository<Document, Integer> {
